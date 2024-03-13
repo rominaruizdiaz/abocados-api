9846e084d002d503c26f548c8dd8b8b4109bc193
Romina Ruiz <ruizdiazsanchezromina@gmail.com>
2024-03-13 22:41:04 +0100

    repository initialized

---
+++ src/test/java/dev/rominaruiz/abocados/AbocadosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rominaruiz.abocados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbocadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
